7674182c2946b3f1c3b70029507c7bce3e5bbe07
imlouiz <luizfelipeazevedobrito1@gmail.com>
2025-05-16 10:51:32 -0300

    Update converters

--- src/model/repository/entity/converter/convert_domain_to_entity.go
+++ src/model/repository/entity/converter/convert_domain_to_entity.go
@@ -5,13 +5,24 @@ import (
 	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/model/repository/entity"
 )
 
-func ConvertDomainToEntity(
-	domain model.UserDomainInterface,
-) *entity.UserEntity {
-	return &entity.UserEntity{
+func ConvertDomainToEntity(domain model.UserDomainInterface) *entity.UserEntity {
+	entity := &entity.UserEntity{
 		Email:    domain.GetEmail(),
 		Password: domain.GetPassword(),
 		Name:     domain.GetName(),
-		Age:      domain.GetAge(),
+		UserType: string(domain.GetUserType()),
 	}
+
+	if domain.GetWorkInfo() != nil {
+		entity.WorkInfo = &entity.WorkInfoEntity{
+			Team:          domain.GetWorkInfo().Team,
+			Position:      domain.GetWorkInfo().Position,
+			DefaultShift:  string(domain.GetWorkInfo().DefaultShift),
+			WeekdayOff:    string(domain.GetWorkInfo().WeekdayOff),
+			WeekendDayOff: string(domain.GetWorkInfo().WeekendDayOff),
+			SuperiorID:    domain.GetWorkInfo().SuperiorID,
+		}
+	}
+
+	return entity
 }
