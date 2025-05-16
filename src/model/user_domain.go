a29e96de2a53b656f84ca7670ab78ec0628a2c25
imlouiz <luizfelipeazevedobrito1@gmail.com>
2025-05-16 10:44:02 -0300

    Update user_domain

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,33 +1,52 @@
 package model
 
+type UserType string
+
+const (
+	UserTypeCollaborator UserType = "colaborador"
+	UserTypeMaster       UserType = "master"
+)
+
+type UserDomainInterface interface {
+	GetID() string
+	GetEmail() string
+	GetPassword() string
+	GetName() string
+	GetUserType() UserType
+	GetWorkInfo() *WorkInfo
+
+	SetID(string)
+	SetWorkInfo(*WorkInfo)
+
+	EncryptPassword()
+}
+
+func NewUserDomain(
+	email, password, name string,
+	userType UserType,
+) UserDomainInterface {
+	return &userDomain{
+		email:    email,
+		password: password,
+		name:     name,
+		userType: userType,
+	}
+}
+
 type userDomain struct {
 	id       string
 	email    string
 	password string
 	name     string
-	age      int
+	userType UserType
+	workInfo *WorkInfo
 }
 
-func (ud *userDomain) GetID() string {
-	return ud.id
-}
-
-func (ud *userDomain) SetID(id string) {
-	ud.id = id
-}
-
-func (ud *userDomain) GetEmail() string {
-	return ud.email
-}
-
-func (ud *userDomain) GetPassword() string {
-	return ud.password
-}
-
-func (ud *userDomain) GetName() string {
-	return ud.name
-}
-
-func (ud *userDomain) GetAge() int {
-	return ud.age
-}
+func (ud *userDomain) GetID() string              { return ud.id }
+func (ud *userDomain) SetID(id string)            { ud.id = id }
+func (ud *userDomain) GetEmail() string           { return ud.email }
+func (ud *userDomain) GetPassword() string        { return ud.password }
+func (ud *userDomain) GetName() string            { return ud.name }
+func (ud *userDomain) GetUserType() UserType      { return ud.userType }
+func (ud *userDomain) GetWorkInfo() *WorkInfo     { return ud.workInfo }
+func (ud *userDomain) SetWorkInfo(info *WorkInfo) { ud.workInfo = info }
