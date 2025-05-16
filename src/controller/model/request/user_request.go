5b2e08dbaceed4a165fbacd2526910bba41a86c6
imlouiz <luizfelipeazevedobrito1@gmail.com>
2025-05-16 10:53:51 -0300

    Update user_request and user_response

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -4,10 +4,10 @@ type UserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6,containsany=!@#$%*"`
 	Name     string `json:"name" binding:"required,min=4,max=100"`
-	Age      int    `json:"age" binding:"required,min=1,max=140"`
+	UserType string `json:"user_type" binding:"required,oneof=colaborador master"`
 }
 
 type UserUpdateRequest struct {
-	Name string `json:"name" binding:"omitempty,min=4,max=100"`
-	Age  int    `json:"age" binding:"omitempty,min=1,max=140"`
+	Name     string `json:"name" binding:"omitempty,min=4,max=100"`
+	UserType string `json:"user_type" binding:"omitempty,oneof=colaborador master"`
 }
--- src/controller/model/response/user_response.go
+++ src/controller/model/response/user_response.go
@@ -1,8 +1,18 @@
 package response
 
 type UserResponse struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
+    ID       string          `json:"id"`
+    Email    string          `json:"email"`
+    Name     string          `json:"name"`
+    UserType string          `json:"user_type"`
+    WorkInfo *WorkInfoResponse `json:"work_info,omitempty"`
+}
+
+type WorkInfoResponse struct {
+    Team          string `json:"team"`
+    Position      string `json:"position"`
+    DefaultShift  string `json:"default_shift"`
+    WeekdayOff    string `json:"weekday_off"`
+    WeekendDayOff string `json:"weekend_day_off"`
+    SuperiorID    string `json:"superior_id"`
 }
