6e17e24a92ae87c5710958d17445b7eda9e5d5e1
Lipe-Azevedo <luizfelipeazevedobrito1@gmail.com>
2025-05-17 21:28:49 -0300

    Fix entity

--- src/model/repository/entity/user_entity.go
+++ src/model/repository/entity/user_entity.go
@@ -3,19 +3,10 @@ package entity
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UserEntity struct {
-    ID       primitive.ObjectID `bson:"_id,omitempty"`
-    Email    string             `bson:"email"`
-    Password string             `bson:"password"`
-    Name     string             `bson:"name"`
-    UserType string             `bson:"user_type"`
-    WorkInfo *WorkInfoEntity    `bson:"work_info,omitempty"`
-}
-
-type WorkInfoEntity struct {
-    Team          string `bson:"team"`
-    Position      string `bson:"position"`
-    DefaultShift  string `bson:"default_shift"`
-    WeekdayOff    string `bson:"weekday_off"`
-    WeekendDayOff string `bson:"weekend_day_off"`
-    SuperiorID    string `bson:"superior_id"`
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Email    string             `bson:"email"`
+	Password string             `bson:"password"`
+	Name     string             `bson:"name"`
+	UserType string             `bson:"user_type"`
+	WorkInfo *WorkInfoEntity    `bson:"work_info,omitempty"`
 }
